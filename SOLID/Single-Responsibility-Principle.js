965e32e60ae726cc15c17e48d02ec74eff45fce5
Ankit Kumar <ankit@Ankits-MacBook-Pro.local>
2019-09-20 00:37:53 +0530

    factory methid

--- SOLID/Single-Responsibility-Principle.js
+++ SOLID/Single-Responsibility-Principle.js
@@ -1,45 +1,42 @@
-const fs = require('fs');
+const fs = require("fs");
 
-class Journal{
-    constructor(){
-        this.entries = [];
-    }
+class Journal {
+  constructor() {
+    this.entries = [];
+  }
 
-    addEntry(e){
-        ++Journal.count;
-        const entry = `${Journal.count}: ${e}`;
-        return this.entries.push(entry);
-    }
+  addEntry(e) {
+    ++Journal.count;
+    const entry = `${Journal.count}: ${e}`;
+    return this.entries.push(entry);
+  }
 
-    removeEntry(index){
-        delete this.entries[index];
-    }
+  removeEntry(index) {
+    delete this.entries[index];
+  }
 
-    toString(){
-        return Object.values(this.entries).join('\n');
-    }
+  toString() {
+    return Object.values(this.entries).join("\n");
+  }
 }
 
 Journal.count = 0;
 
-
-
-class PersistenceManager{
-    constructor(){}
-    saveToFile(journal, filename){
-        fs.writeFileSync(filename, journal.toString());
-    }
-    readFileSync(filename){
-        return fs.readFileSync(filename, {encoding: 'utf-8'});
-    }
+class PersistenceManager {
+  constructor() {}
+  saveToFile(journal, filename) {
+    fs.writeFileSync(filename, journal.toString());
+  }
+  readFileSync(filename) {
+    return fs.readFileSync(filename, { encoding: "utf-8" });
+  }
 }
 
-
-const j =  new Journal();
-j.addEntry('Hello WOrls');
+const j = new Journal();
+j.addEntry("Hello WOrls");
 j.addEntry("qqdwd");
 
 const pManager = new PersistenceManager();
-pManager.saveToFile(j, './test.txt');
+pManager.saveToFile(j, "./test.txt");
 
-console.log(pManager.readFileSync('./test.txt'));
+console.log(pManager.readFileSync("./test.txt"));
